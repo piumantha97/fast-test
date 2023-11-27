ad3a300a6f3257f5fa54a2770af03e1c751db68c
piumantha97 <86832528+piumantha97@users.noreply.github.com>
2023-11-27 12:49:10 +0530

    changes

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ import numpy as np
 import pickle
 import pandas as pd
 import joblib
+from fastapi import HTTPException
 # 2. Create the app object
 app = FastAPI()
 # pickle_in = open("rf_model.pkl","wb")
@@ -68,52 +69,54 @@ def get_name(name: str):
 #    JSON data and return the predicted Bank Note with the confidence
 @app.post('/predict')
 def predict_mobile(data:Mobile):
+    try:
+        print(data.dict())
 
-    print(data.dict())
+        # Convert the Pydantic model to a dictionary
+        data_dict = data.dict()
 
-    # Convert the Pydantic model to a dictionary
-    data_dict = data.dict()
+    # Extract values from the dictionary
+        features = [
+            data_dict['Gender'],
+            data_dict['Age'],
+            data_dict['Education'],
+            data_dict['Occupation'],
+            data_dict['District'],
+            data_dict['CameraSatisfaction'],
+            data_dict['MemorySatisfaction'],
+            data_dict['PerformanceSatisfaction'],
+            data_dict['BatterySatisfaction'],
+            data_dict['spend_amount'],
+            data_dict['Education_purpose'],
+            data_dict['Entertaintment_purpose'],
+            data_dict['Gaming_purpose'],
+            data_dict['Occupation_purpose'],
+            data_dict['Photography_purpose'],
+            data_dict['Socializing_purpose']
+        ]
+    # print(classifier.predict([[variance,skewness,curtosis,entropy]]))
+        # prediction = classifier.predict([[Gender,Age,Education,Occupation,District,CameraSatisfaction,MemorySatisfaction,PerformanceSatisfaction,BatterySatisfaction,spend_amount,Education_purpose,Entertaintment_purpose,Gaming_purpose,Occupation_purpose,Photography_purpose,Socializing_purpose]])
 
- # Extract values from the dictionary
-    features = [
-        data_dict['Gender'],
-        data_dict['Age'],
-        data_dict['Education'],
-        data_dict['Occupation'],
-        data_dict['District'],
-        data_dict['CameraSatisfaction'],
-        data_dict['MemorySatisfaction'],
-        data_dict['PerformanceSatisfaction'],
-        data_dict['BatterySatisfaction'],
-        data_dict['spend_amount'],
-        data_dict['Education_purpose'],
-        data_dict['Entertaintment_purpose'],
-        data_dict['Gaming_purpose'],
-        data_dict['Occupation_purpose'],
-        data_dict['Photography_purpose'],
-        data_dict['Socializing_purpose']
-    ]
-   # print(classifier.predict([[variance,skewness,curtosis,entropy]]))
-    # prediction = classifier.predict([[Gender,Age,Education,Occupation,District,CameraSatisfaction,MemorySatisfaction,PerformanceSatisfaction,BatterySatisfaction,spend_amount,Education_purpose,Entertaintment_purpose,Gaming_purpose,Occupation_purpose,Photography_purpose,Socializing_purpose]])
+        print("Input Features:", features)
 
-    print("Input Features:", features)
+    # Make a prediction
+        # prediction = classifier.predict([features])
+        probabilities = classifier.predict_proba([features])[0]
+        #  return {'prediction': prediction.tolist()} 
+        # Create a list of tuples containing (brand_id, probability)
+        brand_probabilities = list(enumerate(probabilities))
 
-# Make a prediction
-    # prediction = classifier.predict([features])
-    probabilities = classifier.predict_proba([features])[0]
-    #  return {'prediction': prediction.tolist()} 
-    # Create a list of tuples containing (brand_id, probability)
-    brand_probabilities = list(enumerate(probabilities))
+        # Sort the brands based on probabilities in descending order
+        sorted_brands = sorted(brand_probabilities, key=lambda x: x[1], reverse=True)
 
-     # Sort the brands based on probabilities in descending order
-    sorted_brands = sorted(brand_probabilities, key=lambda x: x[1], reverse=True)
+        # Map the numeric prediction to the brand names
+        ranked_brands = [brand_mapping.get(brand_id, "Unknown") for brand_id, _ in sorted_brands]
 
-    # Map the numeric prediction to the brand names
-    ranked_brands = [brand_mapping.get(brand_id, "Unknown") for brand_id, _ in sorted_brands]
-
-    return {'prediction': ranked_brands}
+        return {'prediction': ranked_brands}
 #    return {'prediction': [predicted_brand]}
-
+    except Exception as e:
+        print(f"An error occurred: {str(e)}")
+        raise HTTPException(status_code=500, detail="Internal Server Error")
 
 
 # if __name__ == "__main__":
